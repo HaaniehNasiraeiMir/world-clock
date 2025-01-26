9f98e5ab4fb1193519b67a34a0cba4280eec4699
Haanieh Nasiraei Mir <haaniehnasiraeimir@gmail.com>
2025-01-26 21:48:35 +0330

    added single city

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,8 @@
-alert("hi");
+setInterval(function() {
+let losElement = document.querySelector("#los");
+let losDateElement = losElement.querySelector(".date");
+let losTimeElement = losElement.querySelector(".time");
+let losTime = moment().tz("America/Los_Angeles");
+losDateElement.innerHTML = losTime.format("MMMM Do YYYY");
+losTimeElement.innerHTML = `${losTime.format("h[:]mm[:]ss")} <small>${losTime.format("A")}</small>`;
+}, 1000);
