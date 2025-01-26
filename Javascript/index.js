822fec4f5bb959cdfce0ce9400b5f3642f152117
Haanieh Nasiraei Mir <haaniehnasiraeimir@gmail.com>
2025-01-26 22:36:50 +0330

    added 3 cities time zones

--- Javascript/index.js
+++ Javascript/index.js
@@ -6,3 +6,29 @@ let losTime = moment().tz("America/Los_Angeles");
 losDateElement.innerHTML = losTime.format("MMMM Do YYYY");
 losTimeElement.innerHTML = `${losTime.format("h[:]mm[:]ss")} <small>${losTime.format("A")}</small>`;
 }, 1000);
+setInterval(function () {
+  let parElement = document.querySelector("#par");
+  let parDateElement = parElement.querySelector(".date");
+  let parTimeElement = parElement.querySelector(".time");
+  let parTime = moment().tz("Europe/Paris");
+  parDateElement.innerHTML = parTime.format("MMMM Do YYYY");
+  parTimeElement.innerHTML = `${parTime.format(
+    "h[:]mm[:]ss"
+  )} <small>${parTime.format("A")}</small>`;
+}, 1000);
+function cityChange(event) {
+    let cityTz = event.target.value;
+    let cityName = cityTz.replace('_', ' ').split("/")[1];
+    let cityTime = moment().tz(cityTz);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = ` <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h[:]mm[:]ss"
+        )}<small>${cityTime.format("A")}</small></div>`;
+}
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", cityChange);
